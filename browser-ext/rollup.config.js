5618f92813136118d364d482e12ceca05d56721a
Jens L. <jens@goauthentik.io>
2025-09-04 00:23:56 +0200

    Add browser options page (#239)
    * Add browser options page
    
    * fix missing deps?
    
    * ok actually fix deps
    
    * use profile, lint
    
    * bump
    
    * idk?
    
    * install before lint

--- browser-ext/rollup.config.js
+++ browser-ext/rollup.config.js
@@ -1,16 +1,22 @@
-import typescript from "@rollup/plugin-typescript";
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: {
-    background: "src/background.ts",
-    // popup: 'src/popup.ts'
-  },
-  output: {
-    dir: "dist",
-    format: "es",
-    sourcemap: true,
-  },
-  plugins: [typescript(), resolve(), commonjs()],
+    input: {
+        background: "src/background.ts",
+        options: "src/options.ts",
+    },
+    output: {
+        dir: "dist",
+        format: "es",
+        sourcemap: true,
+    },
+    plugins: [
+        typescript({
+            outputToFilesystem: false,
+        }),
+        resolve(),
+        commonjs(),
+    ],
 };
--- browser-ext/src/background.ts
+++ browser-ext/src/background.ts
@@ -1,7 +1,19 @@
+import { Native } from "./utils/native";
 import { Omnibar } from "./utils/omnibar";
 
 chrome.runtime.onInstalled.addListener(() => {
-  console.debug("authentik Launcher Extension Installed");
+    console.debug("authentik Extension Installed");
 });
 
-new Omnibar().register();
+const native = new Native();
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.action === "get_profiles") {
+        native.listProfiles().then((profiles) => {
+            sendResponse(profiles.profiles);
+        });
+        return true;
+    }
+});
+
+new Omnibar(native).register();
