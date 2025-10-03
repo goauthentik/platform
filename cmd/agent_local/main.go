3b0f46f349935599c2cb81b07e542e27a98149f7
Jens Langhammer <jens.langhammer@beryju.org>
2025-10-03 22:26:17 +0200

    fix sentry

--- cmd/agent_local/main.go
+++ cmd/agent_local/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/getsentry/sentry-go"
 	log "github.com/sirupsen/logrus"
 	agent "goauthentik.io/cli/pkg/agent_local"
+	"goauthentik.io/cli/pkg/storage"
 	"goauthentik.io/cli/pkg/systemlog"
 )
 
@@ -12,10 +16,12 @@ func main() {
 	err := sentry.Init(sentry.ClientOptions{
 		Dsn:              "https://c83cdbb55c9bd568ecfa275932b6de17@o4504163616882688.ingest.us.sentry.io/4509208005312512",
 		TracesSampleRate: 0.3,
+		Release:          fmt.Sprintf("ak-platform-agent-local@%s", storage.FullVersion()),
 	})
 	if err != nil {
 		systemlog.Get().WithError(err).Warn("failed to init sentry")
 	}
+	defer sentry.Flush(2 * time.Second)
 	err = systemlog.Setup("agent")
 	if err != nil {
 		systemlog.Get().WithError(err).Warning("failed to setup logs")
--- cmd/agent_system/main.go
+++ cmd/agent_system/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/getsentry/sentry-go"
 	agentsystem "goauthentik.io/cli/pkg/agent_system"
+	"goauthentik.io/cli/pkg/storage"
+	"goauthentik.io/cli/pkg/systemlog"
 )
 
 func main() {
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:              "https://c83cdbb55c9bd568ecfa275932b6de17@o4504163616882688.ingest.us.sentry.io/4509208005312512",
+		TracesSampleRate: 0.3,
+		Release:          fmt.Sprintf("ak-platform-agent-system@%s", storage.FullVersion()),
+	})
+	if err != nil {
+		systemlog.Get().WithError(err).Warn("failed to init sentry")
+	}
+	defer sentry.Flush(2 * time.Second)
 	agentsystem.Execute()
 }
