09884529fde5942a32b467d7d364f5b0309040f3
Jens Langhammer <jens.langhammer@beryju.org>
2025-10-24 01:44:01 +0200

    bump

--- common.mk
+++ common.mk
@@ -3,7 +3,7 @@ SHELL = /bin/bash
 PWD = $(shell pwd)
 UID = $(shell id -u)
 GID = $(shell id -g)
-VERSION = "0.13.0"
+VERSION = "0.14.0"
 VERSION_HASH = $(shell git rev-parse HEAD)
 ifeq ($(OS),Windows_NT)
 ARCH := $(PROCESSOR_ARCHITEW6432)
