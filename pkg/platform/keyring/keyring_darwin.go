42932a515ae862310e15d9a45aa0e1e1a12ba0f0
Jens Langhammer <jens.langhammer@beryju.org>
2025-10-24 01:38:30 +0200

    agent: rework darwin keychain, again

--- pkg/platform/keyring/keyring_darwin.go
+++ pkg/platform/keyring/keyring_darwin.go
@@ -5,12 +5,14 @@ package keyring
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/keybase/go-keychain"
 	"github.com/pkg/errors"
 )
 
 var ctx *keychain.AuthenticationContext
+var mtx sync.Mutex
 
 const accessGroup = "group.232G855Y8N.io.goauthentik.platform.shared"
 
@@ -22,12 +24,20 @@ func init() {
 
 func baseItem(service string, user string) keychain.Item {
 	item := keychain.NewItem()
-	_ = item.SetAuthenticationContext(ctx)
 	item.SetSecClass(keychain.SecClassGenericPassword)
-	item.SetAccessGroup(accessGroup)
 	item.SetService(service)
 	item.SetAccount(user)
-	item.SetSynchronizable(keychain.SynchronizableNo)
+	item.SetAccessGroup(accessGroup)
+	item.SetAccessible(keychain.AccessibleAfterFirstUnlock)
+	return item
+}
+
+func saveItem(service, user, data string) keychain.Item {
+	item := baseItem(service, user)
+
+	_ = item.SetAuthenticationContext(ctx)
+	item.SetLabel(fmt.Sprintf("authentik Platform SSO: %s", service))
+	item.SetData([]byte(data))
 	return item
 }
 
@@ -40,6 +50,8 @@ func queryItem(service, user string) keychain.Item {
 }
 
 func Get(service string, user string) (string, error) {
+	mtx.Lock()
+	defer mtx.Unlock()
 	results, err := keychain.QueryItem(queryItem(service, user))
 	if err != nil {
 		return "", err
@@ -53,22 +65,36 @@ func Get(service string, user string) (string, error) {
 	return "", keychain.ErrorItemNotFound
 }
 
-func Set(service string, user string, password string) error {
-	item := baseItem(service, user)
-	item.SetLabel(fmt.Sprintf("authentik Platform SSO: %s", service))
-	item.SetData([]byte(password))
+func Set(service string, user string, data string) error {
+	mtx.Lock()
+	defer mtx.Unlock()
+	item := saveItem(service, user, data)
 	err := keychain.AddItem(item)
-	if errors.Is(err, keychain.ErrorDuplicateItem) {
-		return keychain.UpdateItem(queryItem(service, user), item)
+	if err != nil {
+		if errors.Is(err, keychain.ErrorDuplicateItem) {
+			err = keychain.UpdateItem(queryItem(service, user), item)
+			if err != nil {
+				return errors.Wrap(err, "failed to update item")
+			}
+			return nil
+		} else {
+			return errors.Wrap(err, "failed to add item")
+		}
 	}
-	return err
+	return nil
+}
+
+func Delete(service string, user string) error {
+	mtx.Lock()
+	defer mtx.Unlock()
+	item := queryItem(service, user)
+	err := keychain.DeleteItem(item)
+	if err != nil {
+		return errors.Wrap(err, "failed to delete item")
+	}
+	return nil
 }
 
 func IsNotExist(err error) bool {
 	return errors.Is(err, keychain.ErrorItemNotFound)
-}
-
-func Delete(service string, user string) error {
-	item := baseItem(service, user)
-	return keychain.DeleteItem(item)
 }
