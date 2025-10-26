2dab2340508b2475ad37d5f9dd8c91d7c32549a0
Jens Langhammer <jens.langhammer@beryju.org>
2025-10-26 15:54:24 +0100

    fix lint

--- vnd/go-touchid/touchid_linux.go
+++ vnd/go-touchid/touchid_linux.go
@@ -3,7 +3,7 @@ package touchid
 import "errors"
 
 // ErrTouchIDNotSupported is the error returned when Touch ID is not supported on the current platform
-var ErrTouchIDNotSupported = errors.New("Touch ID authentication is not supported on this platform")
+var ErrTouchIDNotSupported = errors.New("touch ID authentication is not supported on this platform")
 
 // Authenticate attempts to authenticate using Touch ID
 // On Linux, this always returns false with an error indicating lack of support
