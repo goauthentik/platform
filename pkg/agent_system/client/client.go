cb466967c93409bad8c57b3ffd56524a1301738e
Jens Langhammer <jens.langhammer@beryju.org>
2025-10-27 14:35:45 +0100

    sysd: use sentry in grpc client

--- pkg/agent_system/client/client.go
+++ pkg/agent_system/client/client.go
@@ -5,6 +5,7 @@ import (
 	"net"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	grpc_sentry "github.com/johnbellone/grpc-middleware-sentry"
 	log "github.com/sirupsen/logrus"
 	"goauthentik.io/platform/pkg/agent_system/types"
 	"goauthentik.io/platform/pkg/pb"
@@ -29,8 +30,18 @@ func New() (*SysdClient, error) {
 		grpc.WithContextDialer(func(ctx context.Context, s string) (net.Conn, error) {
 			return socket.Connect(types.GetSysdSocketPath())
 		}),
-		grpc.WithUnaryInterceptor(logging.UnaryClientInterceptor(systemlog.InterceptorLogger(l))),
-		grpc.WithStreamInterceptor(logging.StreamClientInterceptor(systemlog.InterceptorLogger(l))),
+		grpc.WithChainUnaryInterceptor(
+			logging.UnaryClientInterceptor(systemlog.InterceptorLogger(l)),
+			grpc_sentry.UnaryClientInterceptor(grpc_sentry.WithReportOn(func(error) bool {
+				return false
+			})),
+		),
+		grpc.WithChainStreamInterceptor(
+			logging.StreamClientInterceptor(systemlog.InterceptorLogger(l)),
+			grpc_sentry.StreamClientInterceptor(grpc_sentry.WithReportOn(func(error) bool {
+				return false
+			})),
+		),
 	)
 	if err != nil {
 		return nil, err
