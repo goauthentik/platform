0ea7cc16555809b69be5e16e163fb5ba2a9f50fe
unknown <jens@goauthentik.io>
2025-10-29 22:34:04 +0000

    the wrong solution

--- hack/windows/setup.sh
+++ hack/windows/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 export PATH="/c/Program Files (x86)/GnuWin32/bin/":$PATH
 export PATH="/c/Program Files/Go/bin/":$PATH
-export PATH="/c/Strawberry/c/bin/":$PATH
+# export PATH="/c/Strawberry/c/bin/":$PATH
 which cmake
