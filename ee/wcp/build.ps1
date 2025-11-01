ad5cfc33c457471f26074ad4a9d6e38709e9f10c
unknown <jens@goauthentik.io>
2025-11-01 23:23:13 +0000

    only import paths not in ci

--- ee/wcp/build.ps1
+++ ee/wcp/build.ps1
@@ -1,6 +1,10 @@
 Set-PSDebug -Trace 1
 $ErrorActionPreference = "Stop"
 
+if ($env:CI -ne "true") {
+    . "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1"
+}
+
 $pwd = $args[0]
 $top = $args[1]
 $target = $args[2]
