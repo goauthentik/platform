03088b8f53f7a947b4c70616f652a90f9e8a6c19
unknown <jens@goauthentik.io>
2025-11-01 22:05:39 +0000

    fix

--- hack/windows/setup.sh
+++ hack/windows/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -xe
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 export PATH="/c/Program Files (x86)/GnuWin32/bin/":$PATH
 export PATH="/c/Program Files/Go/bin/":$PATH
-eval "$(./hack/windows/vcvarsall.sh x64)"
+eval "$($SCRIPT_DIR/vcvarsall.sh x64)"
 # export PATH="/c/Strawberry/c/bin/":$PATH
 which cmake
