71b0522fbe184ca93c71ae08871e0ac78481e3bd
Jens Langhammer <jens.langhammer@beryju.org>
2025-11-01 22:11:07 +0100

    update cache location

--- ee/wcp/build.ps1
+++ ee/wcp/build.ps1
@@ -1,9 +1,12 @@
 Set-PSDebug -Trace 1
 New-Item -ea 0 -Force -ItemType Directory "bin/wcp"
+New-Item -ea 0 -Force -ItemType Directory "cache/wcp"
 . hack/windows/setup.ps1
 nmake /P
 pwd
-cd "./bin/wcp"
+cd "./cache/wcp"
 pwd
 cmake --debug-find -G "Visual Studio 17" "../../ee/wcp"
 cmake --build . --config Release
+cd ../..
+cp "./cache/wcp/Release/*" "bin/wcp/"
