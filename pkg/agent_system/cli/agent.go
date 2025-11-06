533e23fd3254dea23ac177266143275940509b5f
Jens Langhammer <jens.langhammer@beryju.org>
2025-11-06 22:50:15 +0100

    less duplicate code

--- pkg/agent_system/cli/agent.go
+++ pkg/agent_system/cli/agent.go
@@ -1,17 +1,16 @@
-//go:build !windows
-
 package cli
 
 import (
 	"os"
 
 	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	agentsystem "goauthentik.io/platform/pkg/agent_system"
 	"goauthentik.io/platform/pkg/agent_system/config"
 	systemlog "goauthentik.io/platform/pkg/platform/log"
+	"goauthentik.io/platform/pkg/platform/pstr"
 )
+
+var isDebug = false
 
 var agentCmd = &cobra.Command{
 	Use:          "agent",
@@ -25,24 +24,21 @@ var agentCmd = &cobra.Command{
 		if _, err := os.Stat(config.Manager().Get().RuntimeDir); err != nil {
 			return errors.Wrap(err, "failed to check runtime directory")
 		}
-		err = systemlog.Setup("sysd")
+		err = systemlog.Setup(pstr.PlatformString{
+			Windows: pstr.S("authentik Sysd"),
+			Linux:   pstr.S("sysd"),
+		}.ForCurrent())
 		if err != nil {
 			return err
 		}
 		return nil
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		defer systemlog.Cleanup()
-		log.SetLevel(log.DebugLevel)
-		agent, err := agentsystem.New()
-		if err != nil {
-			return err
-		}
-		agent.Start()
-		return nil
+		return runAgentPlatform(cmd, args)
 	},
 }
 
 func init() {
+	agentCmd.Flags().BoolVarP(&isDebug, "debug", "d", false, "Run in debug mode.")
 	rootCmd.AddCommand(agentCmd)
 }
