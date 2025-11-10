eedf1840c843b6a0a109724ebd02d749a754a201
Jens Langhammer <jens.langhammer@beryju.org>
2025-11-10 23:51:31 +0100

    fix pam not running on windows

--- pkg/agent_system/components_windows.go
+++ pkg/agent_system/components_windows.go
@@ -1,18 +1,20 @@
-//go:build !linux
+//go:build windows
 
 package agentsystem
 
 import (
-	agentstarter "goauthentik.io/platform/pkg/agent_system/agent_starter"
 	"goauthentik.io/platform/pkg/agent_system/component"
 	"goauthentik.io/platform/pkg/agent_system/device"
+	"goauthentik.io/platform/pkg/agent_system/pam"
 	"goauthentik.io/platform/pkg/agent_system/ping"
+	"goauthentik.io/platform/pkg/agent_system/session"
 )
 
 func (sm *SystemAgent) RegisterPlatformComponents() map[string]component.Constructor {
 	return map[string]component.Constructor{
-		device.ID:       device.NewServer,
-		ping.ID:         ping.NewServer,
-		agentstarter.ID: agentstarter.NewServer,
+		device.ID:  device.NewServer,
+		session.ID: session.NewMonitor,
+		pam.ID:     pam.NewServer,
+		ping.ID:    ping.NewServer,
 	}
 }
