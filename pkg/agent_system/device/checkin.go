f63aa9b162a3598b741269c3c258f37a6ca0a335
Jens Langhammer <jens.langhammer@beryju.org>
2025-11-17 14:53:12 +0100

    logging for gather timing

--- pkg/agent_system/device/checkin.go
+++ pkg/agent_system/device/checkin.go
@@ -8,11 +8,13 @@ import (
 )
 
 func (ds *Server) checkIn() {
-	req, err := facts.Gather()
+	ds.log.Debug("Starting facts gathering...")
+	req, err := facts.Gather(ds.log)
 	if err != nil {
 		ds.log.WithError(err).Warning("failed to gather device info")
 		return
 	}
+	ds.log.Debug("Finished facts gathering")
 	hr, err := ds.api.EndpointsApi.EndpointsAgentsConnectorsCheckInCreate(context.Background()).DeviceFactsRequest(*req).Execute()
 	if err != nil {
 		ds.log.WithError(ak.HTTPToError(hr, err)).Warning("failed to checkin")
--- pkg/platform/facts/facts.go
+++ pkg/platform/facts/facts.go
@@ -1,6 +1,7 @@
 package facts
 
 import (
+	log "github.com/sirupsen/logrus"
 	"goauthentik.io/api/v3"
 	"goauthentik.io/platform/pkg/meta"
 	"goauthentik.io/platform/pkg/platform/facts/disk"
@@ -11,27 +12,32 @@ import (
 )
 
 // Gather collects system information from all subsystems
-func Gather() (*api.DeviceFactsRequest, error) {
+func Gather(log *log.Entry) (*api.DeviceFactsRequest, error) {
+	log.WithField("area", "disk").Debug("Gathering...")
 	disks, err := disk.Gather()
 	if err != nil {
 		return nil, err
 	}
 
+	log.WithField("area", "hardware").Debug("Gathering...")
 	hw, err := hardware.Gather()
 	if err != nil {
 		return nil, err
 	}
 
+	log.WithField("area", "network").Debug("Gathering...")
 	net, err := network.Gather()
 	if err != nil {
 		return nil, err
 	}
 
+	log.WithField("area", "os").Debug("Gathering...")
 	osInfo, err := os.Gather()
 	if err != nil {
 		return nil, err
 	}
 
+	log.WithField("area", "process").Debug("Gathering...")
 	procs, err := process.Gather()
 	if err != nil {
 		return nil, err
