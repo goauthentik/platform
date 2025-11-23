aee18c5e94a6c54e8e5f427939c80d62990ecf4e
Jens Langhammer <jens.langhammer@beryju.org>
2025-11-23 13:35:16 +0100

    platform: switch to windows etw logging

--- pkg/platform/log/logs_windows.go
+++ pkg/platform/log/logs_windows.go
@@ -3,71 +3,16 @@
 package log
 
 import (
-	"encoding/json"
-	"io"
+	"os"
 
+	"github.com/Microsoft/go-winio/pkg/etwlogrus"
 	log "github.com/sirupsen/logrus"
-	"golang.org/x/sys/windows/svc/eventlog"
 )
 
-// Write log messages to the Windows event log.
-type eventLogHook struct {
-	log *eventlog.Log
-}
-
-func newEventLogHook(src string) (*eventLogHook, error) {
-	e, err := eventlog.Open(src)
-	if err != nil {
-		return nil, err
-	}
-	return &eventLogHook{
-		log: e,
-	}, nil
-}
-
-func (e *eventLogHook) Levels() []log.Level {
-	return []log.Level{
-		log.DebugLevel,
-		log.ErrorLevel,
-		log.InfoLevel,
-		log.WarnLevel,
-		log.TraceLevel,
-	}
-}
-
-const windowsEventID = 1000
-
-func (e *eventLogHook) Fire(entry *log.Entry) error {
-	msg := entry.Message
-	if len(entry.Data) > 0 {
-		msg += "\n\n\n"
-		v, err := json.Marshal(entry.Data)
-		if err != nil {
-			return err
-		}
-		msg += string(v)
-	}
-	switch entry.Level {
-	case log.InfoLevel:
-		return e.log.Info(windowsEventID, msg)
-	case log.WarnLevel:
-		return e.log.Warning(windowsEventID, msg)
-	case log.ErrorLevel:
-		return e.log.Error(windowsEventID, msg)
-	default:
-		// Fallback to info if we don't have a level-mapping
-		return e.log.Info(windowsEventID, msg)
-	}
-}
-
-func (e *eventLogHook) close() error {
-	return e.log.Close()
-}
-
-var h *eventLogHook
+var h *etwlogrus.Hook
 
 func platformSetup(appName string) error {
-	hook, err := newEventLogHook(appName)
+	hook, err := etwlogrus.NewHook(appName)
 	if err != nil {
 		return err
 	}
@@ -78,14 +23,14 @@ func platformSetup(appName string) error {
 		DisableSorting:   true,
 	})
 	log.StandardLogger().Hooks.Add(h)
-	log.StandardLogger().SetOutput(io.Discard)
+	log.StandardLogger().SetOutput(os.Stdout)
 	log.Info("Switched to windows EventLog logging...")
 	return nil
 }
 
 func platformCleanup() error {
 	if h != nil {
-		return h.close()
+		return h.Close()
 	}
 	return nil
 }
