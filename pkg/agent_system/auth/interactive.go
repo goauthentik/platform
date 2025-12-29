f9fbf6cf4478eb7c99c86e3d6b5d185a9f689e21
Jens Langhammer <jens.langhammer@beryju.org>
2025-12-30 00:10:07 +0100

    rework interactive

--- pkg/agent_system/auth/interactive.go
+++ pkg/agent_system/auth/interactive.go
@@ -6,20 +6,9 @@ import (
 	"errors"
 
 	"github.com/gorilla/securecookie"
-	"goauthentik.io/api/v3"
 	"goauthentik.io/platform/pkg/ak/flow"
 	"goauthentik.io/platform/pkg/pb"
 )
-
-type InteractiveAuthTransaction struct {
-	ctx      context.Context
-	cancel   context.CancelFunc
-	ID       string
-	fex      *flow.FlowExecutor
-	username string
-	password string
-	result   *pb.InteractiveAuthResult
-}
 
 func (auth *Server) InteractiveAuth(ctx context.Context, req *pb.InteractiveAuthRequest) (*pb.InteractiveChallenge, error) {
 	var ch *pb.InteractiveChallenge
@@ -34,13 +23,21 @@ func (auth *Server) InteractiveAuth(ctx context.Context, req *pb.InteractiveAuth
 
 func (auth *Server) interactiveAuthInit(_ context.Context, req *pb.InteractiveAuthInitRequest) (*pb.InteractiveChallenge, error) {
 	id := base64.StdEncoding.EncodeToString(securecookie.GenerateRandomKey(64))
+	api, err := auth.dom.APIClient()
+	if err != nil {
+		auth.log.WithError(err).Warning("failed to get API client for doamin")
+		return nil, err
+	}
 	txn := &InteractiveAuthTransaction{
 		ID:       id,
 		username: req.Username,
 		password: req.Password,
+		api:      api,
+		log:      auth.log.WithField("txn", id),
+		dom:      auth.dom,
 	}
 	txn.ctx, txn.cancel = context.WithCancel(auth.ctx.Context())
-	fex, err := flow.NewFlowExecutor(txn.ctx, *auth.dom.Config().AuthorizationFlow.Get(), auth.api.GetConfig(), flow.FlowExecutorOptions{
+	fex, err := flow.NewFlowExecutor(txn.ctx, *auth.dom.Config().AuthorizationFlow.Get(), txn.api.GetConfig(), flow.FlowExecutorOptions{
 		Logger: func(msg string, fields map[string]any) {
 			auth.log.WithField("logger", "component.pam.flow").WithFields(fields).Info(msg)
 		},
@@ -56,7 +53,7 @@ func (auth *Server) interactiveAuthInit(_ context.Context, req *pb.InteractiveAu
 	auth.m.Lock()
 	defer auth.m.Unlock()
 	auth.txns[id] = txn
-	return auth.getNextChallenge(txn)
+	return txn.getNextChallenge()
 }
 
 func (auth *Server) interactiveAuthContinue(_ context.Context, req *pb.InteractiveAuthContinueRequest) (*pb.InteractiveChallenge, error) {
@@ -75,113 +72,12 @@ func (auth *Server) interactiveAuthContinue(_ context.Context, req *pb.Interacti
 			SessionId: base64.StdEncoding.EncodeToString(securecookie.GenerateRandomKey(64)),
 		}, nil
 	}
-	c, err := auth.solveChallenge(txn, req)
+	c, err := txn.solveChallenge(req)
 	if c != nil {
 		return c, nil
 	}
 	if err != nil {
 		return nil, err
 	}
-	return auth.getNextChallenge(txn)
-}
-
-func (auth *Server) getNextChallenge(txn *InteractiveAuthTransaction) (*pb.InteractiveChallenge, error) {
-	c := &pb.InteractiveChallenge{
-		Txid: txn.ID,
-	}
-	nc := txn.fex.Challenge()
-	i := nc.GetActualInstance()
-	if i == nil {
-		return nil, errors.New("response request instance was null")
-	}
-	ch := i.(flow.ChallengeCommon)
-
-	switch ch.GetComponent() {
-	case string(flow.StageRedirect):
-		txn.result = pb.InteractiveAuthResult_PAM_SUCCESS.Enum()
-		return &pb.InteractiveChallenge{
-			Txid:      txn.ID,
-			Finished:  true,
-			Result:    pb.InteractiveAuthResult_PAM_SUCCESS,
-			SessionId: base64.StdEncoding.EncodeToString(securecookie.GenerateRandomKey(64)),
-		}, nil
-	case string(flow.StageAccessDenied):
-		txn.result = pb.InteractiveAuthResult_PAM_PERM_DENIED.Enum()
-		return &pb.InteractiveChallenge{
-			Txid:       txn.ID,
-			Finished:   true,
-			Result:     pb.InteractiveAuthResult_PAM_PERM_DENIED,
-			Prompt:     *nc.AccessDeniedChallenge.ErrorMessage,
-			PromptMeta: pb.InteractiveChallenge_PAM_ERROR_MSG,
-		}, nil
-	case string(flow.StageIdentification):
-		cc := nc.IdentificationChallenge
-		if !cc.PasswordFields {
-			// No password field, only identification -> directly answer
-			c, err := auth.solveChallenge(txn, &pb.InteractiveAuthContinueRequest{
-				Value: txn.username,
-			})
-			if c != nil {
-				return c, nil
-			}
-			if err != nil {
-				return nil, err
-			}
-			return auth.getNextChallenge(txn)
-		}
-	case string(flow.StagePassword):
-		if txn.password != "" {
-			c, err := auth.solveChallenge(txn, &pb.InteractiveAuthContinueRequest{
-				Value: txn.password,
-			})
-			txn.password = ""
-			if c != nil {
-				return c, nil
-			}
-			if err != nil {
-				return nil, err
-			}
-			return auth.getNextChallenge(txn)
-		}
-		return &pb.InteractiveChallenge{
-			Txid:       txn.ID,
-			Prompt:     "authentik Password: ",
-			PromptMeta: pb.InteractiveChallenge_PAM_PROMPT_ECHO_OFF,
-		}, nil
-	default:
-		auth.log.WithField("component", ch.GetComponent()).Warning("unsupported stage type")
-	}
-	return c, nil
-}
-
-func (auth *Server) solveChallenge(txn *InteractiveAuthTransaction, req *pb.InteractiveAuthContinueRequest) (*pb.InteractiveChallenge, error) {
-	nc := txn.fex.Challenge()
-	i := nc.GetActualInstance()
-	if i == nil {
-		return nil, errors.New("response request instance was null")
-	}
-	ch := i.(flow.ChallengeCommon)
-
-	freq := &api.FlowChallengeResponseRequest{}
-	switch ch.GetComponent() {
-	case string(flow.StageIdentification):
-		freq.IdentificationChallengeResponseRequest = &api.IdentificationChallengeResponseRequest{
-			UidField: *api.NewNullableString(api.PtrString(req.Value)),
-		}
-	case string(flow.StagePassword):
-		freq.PasswordChallengeResponseRequest = &api.PasswordChallengeResponseRequest{
-			Password: req.Value,
-		}
-	default:
-		auth.log.WithField("component", ch.GetComponent()).Warning("unsupported stage type")
-	}
-	_, err := txn.fex.SolveFlowChallenge(freq)
-	if err != nil {
-		return &pb.InteractiveChallenge{
-			Txid:       txn.ID,
-			Prompt:     err.Error(),
-			PromptMeta: pb.InteractiveChallenge_PAM_ERROR_MSG,
-		}, err
-	}
-	return nil, nil
+	return txn.getNextChallenge()
 }
---
+++ pkg/agent_system/auth/interactive_txn.go
@@ -0,0 +1,162 @@
+package auth
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/securecookie"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	"goauthentik.io/api/v3"
+	"goauthentik.io/platform/pkg/agent_system/config"
+	"goauthentik.io/platform/pkg/ak"
+	"goauthentik.io/platform/pkg/ak/flow"
+	"goauthentik.io/platform/pkg/pb"
+)
+
+type InteractiveAuthTransaction struct {
+	ctx      context.Context
+	cancel   context.CancelFunc
+	ID       string
+	fex      *flow.FlowExecutor
+	username string
+	password string
+	result   *pb.InteractiveAuthResult
+	api      *api.APIClient
+	log      *log.Entry
+	dom      *config.DomainConfig
+}
+
+func (txn *InteractiveAuthTransaction) getNextChallenge() (*pb.InteractiveChallenge, error) {
+	c := &pb.InteractiveChallenge{
+		Txid: txn.ID,
+	}
+	nc := txn.fex.Challenge()
+	i := nc.GetActualInstance()
+	if i == nil {
+		return nil, errors.New("response request instance was null")
+	}
+	ch := i.(flow.ChallengeCommon)
+
+	switch ch.GetComponent() {
+	case string(flow.StageRedirect):
+		return txn.finishSuccess()
+	case string(flow.StageAccessDenied):
+		txn.result = pb.InteractiveAuthResult_PAM_PERM_DENIED.Enum()
+		return &pb.InteractiveChallenge{
+			Txid:       txn.ID,
+			Finished:   true,
+			Result:     pb.InteractiveAuthResult_PAM_PERM_DENIED,
+			Prompt:     *nc.AccessDeniedChallenge.ErrorMessage,
+			PromptMeta: pb.InteractiveChallenge_PAM_ERROR_MSG,
+		}, nil
+	case string(flow.StageIdentification):
+		cc := nc.IdentificationChallenge
+		if !cc.PasswordFields {
+			// No password field, only identification -> directly answer
+			c, err := txn.solveChallenge(&pb.InteractiveAuthContinueRequest{
+				Value: txn.username,
+			})
+			if c != nil {
+				return c, nil
+			}
+			if err != nil {
+				return nil, err
+			}
+			return txn.getNextChallenge()
+		}
+	case string(flow.StagePassword):
+		if txn.password != "" {
+			c, err := txn.solveChallenge(&pb.InteractiveAuthContinueRequest{
+				Value: txn.password,
+			})
+			txn.password = ""
+			if c != nil {
+				return c, nil
+			}
+			if err != nil {
+				return nil, err
+			}
+			return txn.getNextChallenge()
+		}
+		return &pb.InteractiveChallenge{
+			Txid:       txn.ID,
+			Prompt:     "authentik Password: ",
+			PromptMeta: pb.InteractiveChallenge_PAM_PROMPT_ECHO_OFF,
+		}, nil
+	default:
+		txn.log.WithField("component", ch.GetComponent()).Warning("unsupported stage type")
+	}
+	return c, nil
+}
+
+func (txn *InteractiveAuthTransaction) solveChallenge(req *pb.InteractiveAuthContinueRequest) (*pb.InteractiveChallenge, error) {
+	nc := txn.fex.Challenge()
+	i := nc.GetActualInstance()
+	if i == nil {
+		return nil, errors.New("response request instance was null")
+	}
+	ch := i.(flow.ChallengeCommon)
+
+	freq := &api.FlowChallengeResponseRequest{}
+	switch ch.GetComponent() {
+	case string(flow.StageIdentification):
+		freq.IdentificationChallengeResponseRequest = &api.IdentificationChallengeResponseRequest{
+			UidField: *api.NewNullableString(api.PtrString(req.Value)),
+		}
+	case string(flow.StagePassword):
+		freq.PasswordChallengeResponseRequest = &api.PasswordChallengeResponseRequest{
+			Password: req.Value,
+		}
+	default:
+		txn.log.WithField("component", ch.GetComponent()).Warning("unsupported stage type")
+	}
+	_, err := txn.fex.SolveFlowChallenge(freq)
+	if err != nil {
+		return &pb.InteractiveChallenge{
+			Txid:       txn.ID,
+			Prompt:     err.Error(),
+			PromptMeta: pb.InteractiveChallenge_PAM_ERROR_MSG,
+		}, err
+	}
+	return nil, nil
+}
+
+func (txn *InteractiveAuthTransaction) doInteractiveAuth(url string) (any, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer+agent %s", txn.dom.Token))
+	res, err := txn.api.GetConfig().HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if res.Request.URL.Scheme == "goauthentik.io" && res.Request.URL.Host == "platform" && res.Request.URL.Path == "/finished" {
+		return "foo", nil
+	}
+	return nil, fmt.Errorf("idk")
+}
+
+func (txn *InteractiveAuthTransaction) finishSuccess() (*pb.InteractiveChallenge, error) {
+	res, hr, err := txn.api.EndpointsApi.EndpointsAgentsConnectorsAuthIaCreate(txn.ctx).Execute()
+	if err != nil {
+		return nil, ak.HTTPToError(hr, err)
+	}
+
+	code, err := txn.doInteractiveAuth(res.Url)
+	if err != nil {
+		return nil, err
+	}
+	txn.log.Debug(code)
+
+	txn.result = pb.InteractiveAuthResult_PAM_SUCCESS.Enum()
+	return &pb.InteractiveChallenge{
+		Txid:      txn.ID,
+		Finished:  true,
+		Result:    pb.InteractiveAuthResult_PAM_SUCCESS,
+		SessionId: base64.StdEncoding.EncodeToString(securecookie.GenerateRandomKey(64)),
+	}, nil
+}
