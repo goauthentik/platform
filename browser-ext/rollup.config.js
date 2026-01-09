54f73b93fd4f3699480d47da6de5c90110a84dfd
Jens Langhammer <jens.langhammer@beryju.org>
2026-01-09 01:07:48 +0100

    format

--- browser-ext/rollup.config.js
+++ browser-ext/rollup.config.js
@@ -1,7 +1,7 @@
+import { codecovRollupPlugin } from "@codecov/rollup-plugin";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import { codecovRollupPlugin } from "@codecov/rollup-plugin";
 
 export default {
     input: {
