acc5f39cb32769efc8a3cd9450aa12f14219419e
Jens Langhammer <jens@goauthentik.io>
2026-01-10 13:49:28 +0000

    rotate logs

--- ee/wcp/ak_cred_provider/ak_log.cpp
+++ ee/wcp/ak_cred_provider/ak_log.cpp
@@ -3,17 +3,21 @@
 #include "ak_log.h"
 #include "ak_version.h"
 #include "spdlog/async.h"
-#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/rotating_file_sink.h"
 #include "spdlog/sinks/win_eventlog_sink.h"
 #include "spdlog/spdlog.h"
 #include <string>
 
-bool g_logSetup;;
+bool g_logSetup;
+
+const auto _ak_log_max_size = 1024 * 1024 * 50;
+const auto _ak_log_max_files = 3;
 
 void SetupLogs(const char* logger_name) {
-  const auto logger = spdlog::basic_logger_mt(
+  const auto logger = spdlog::rotating_logger_mt(
       logger_name,
-      std::string(AK_PROGRAM_DATA).append("\\logs\\").append(logger_name).append(".log").c_str());
+      std::string(AK_PROGRAM_DATA).append("\\logs\\").append(logger_name).append(".log").c_str(),
+      _ak_log_max_size, _ak_log_max_files);
   spdlog::set_level(spdlog::level::debug);
   spdlog::flush_every(std::chrono::seconds(5));
   spdlog::set_default_logger(logger);
