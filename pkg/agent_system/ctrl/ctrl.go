feba12cc3ff54ed847ac4d3e9bc43a2b8bfa8e43
Jens Langhammer <jens.langhammer@beryju.org>
2026-03-01 15:04:23 +0100

    sysd: fix incorrect display in troubleshoot

--- pkg/agent_system/ctrl/ctrl.go
+++ pkg/agent_system/ctrl/ctrl.go
@@ -3,8 +3,10 @@ package ctrl
 import (
 	log "github.com/sirupsen/logrus"
 	"goauthentik.io/platform/pkg/agent_system/component"
+	"goauthentik.io/platform/pkg/agent_system/config"
 	"goauthentik.io/platform/pkg/agent_system/types"
 	"goauthentik.io/platform/pkg/pb"
+	"goauthentik.io/platform/pkg/storage/state"
 	"google.golang.org/grpc"
 )
 
@@ -15,12 +17,14 @@ type Server struct {
 
 	log *log.Entry
 	ctx component.Context
+	rst *state.State
 }
 
 func NewServer(ctx component.Context) (component.Component, error) {
 	return &Server{
 		log: ctx.Log(),
 		ctx: ctx,
+		rst: config.State(),
 	}, nil
 }
 
