f98e8227f977af63d9a67cdad60faf683c473a5d
Jens Langhammer <jens.langhammer@beryju.org>
2026-03-04 21:22:26 +0100

    sysd: only report on specified error codes

--- pkg/shared/grpc.go
+++ pkg/shared/grpc.go
@@ -1,6 +1,8 @@
 package shared
 
 import (
+	"slices"
+
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
 	grpc_sentry "github.com/johnbellone/grpc-middleware-sentry"
@@ -13,30 +15,41 @@ import (
 )
 
 func CommonGRPCServerOpts(l *log.Entry, extra ...grpc.ServerOption) []grpc.ServerOption {
+	allowedErrorCodes := []codes.Code{
+		codes.Internal,
+		codes.Unimplemented,
+		codes.Unknown,
+	}
+	sentryOpts := []grpc_sentry.Option{
+		grpc_sentry.WithRepanicOption(true),
+		grpc_sentry.WithReportOn(func(err error) bool {
+			st, ok := status.FromError(err)
+			if ok {
+				return slices.Contains(allowedErrorCodes, st.Code())
+			}
+			return true
+		}),
+	}
+	recoveryOpts := []recovery.Option{
+		recovery.WithRecoveryHandler(func(p any) (err error) {
+			if e, ok := p.(error); ok {
+				l.WithError(e).Warning("GRPC method panic'd")
+			} else {
+				l.WithField("p", p).Warning("GRPC method panic'd")
+			}
+			return status.Errorf(codes.Unknown, "panic triggered")
+		}),
+	}
 	opts := []grpc.ServerOption{
 		grpc.ChainUnaryInterceptor(
 			logging.UnaryServerInterceptor(systemlog.InterceptorLogger(l)),
-			grpc_sentry.UnaryServerInterceptor(grpc_sentry.WithRepanicOption(true)),
-			recovery.UnaryServerInterceptor(recovery.WithRecoveryHandler(func(p any) (err error) {
-				if e, ok := p.(error); ok {
-					l.WithError(e).Warning("GRPC method panic'd")
-				} else {
-					l.WithField("p", p).Warning("GRPC method panic'd")
-				}
-				return status.Errorf(codes.Unknown, "panic triggered")
-			})),
+			grpc_sentry.UnaryServerInterceptor(sentryOpts...),
+			recovery.UnaryServerInterceptor(recoveryOpts...),
 		),
 		grpc.ChainStreamInterceptor(
 			logging.StreamServerInterceptor(systemlog.InterceptorLogger(l)),
-			grpc_sentry.StreamServerInterceptor(grpc_sentry.WithRepanicOption(true)),
-			recovery.StreamServerInterceptor(recovery.WithRecoveryHandler(func(p any) (err error) {
-				if e, ok := p.(error); ok {
-					l.WithError(e).Warning("GRPC method panic'd")
-				} else {
-					l.WithField("p", p).Warning("GRPC method panic'd")
-				}
-				return status.Errorf(codes.Unknown, "panic triggered")
-			})),
+			grpc_sentry.StreamServerInterceptor(sentryOpts...),
+			recovery.StreamServerInterceptor(recoveryOpts...),
 		),
 	}
 	return append(opts, extra...)
