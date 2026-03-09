253510b198c93beb9a13f4d97fea17b81fdc2dde
Jens Langhammer <jens.langhammer@beryju.org>
2026-03-09 01:19:47 +0100

    statically disable components

--- cmd/agent_system/mobilebind/agent.go
+++ cmd/agent_system/mobilebind/agent.go
@@ -4,6 +4,9 @@ import (
 	"path"
 
 	agentsystem "goauthentik.io/platform/pkg/agent_system"
+	agentstarter "goauthentik.io/platform/pkg/agent_system/agent_starter"
+	"goauthentik.io/platform/pkg/agent_system/directory"
+	"goauthentik.io/platform/pkg/agent_system/session"
 	"goauthentik.io/platform/pkg/platform/pstr"
 )
 
@@ -11,7 +14,11 @@ var agent *agentsystem.SystemAgent
 
 func Init() bool {
 	a, err := agentsystem.New(agentsystem.SystemAgentOptions{
-		DisabledComponents: []string{},
+		DisabledComponents: []string{
+			agentstarter.ID,
+			directory.ID,
+			session.ID,
+		},
 		SocketPath: func(id string) pstr.PlatformString {
 			return pstr.PlatformString{
 				Fallback: path.Join(tempRoot, id+".sock"),
