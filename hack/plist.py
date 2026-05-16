b949e6fe66d315a37e93726213f9f745e2b2da69
Jens Langhammer <jens.langhammer@beryju.org>
2026-05-16 20:19:15 +0200

    ci: replace plistbuddy with python script

---
+++ hack/plist.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from plistlib import loads, dumps
+from sys import argv
+
+
+if len(argv) < 3:
+    print("""Small helper script to edit plist files, since `Plistbuddy` is macOS only
+        Arguments:
+          - plist file to edit
+          - key to modify
+          - value to set""")
+    exit()
+
+with open(argv[1], "r+") as _plist:
+    data = loads(_plist.read())
+    data[argv[2]] = argv[3]
+    _plist.seek(0)
+    _plist.write(dumps(data).decode())
