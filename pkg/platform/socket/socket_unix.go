fd033ea958d1855218930e20aae08b988b310881
Jens Langhammer <jens.langhammer@beryju.org>
2026-05-17 17:54:29 +0200

    Revert "platform/socket: prevent small window of socket having incorrect permissions"
    This reverts commit e4c6cbc68669726bd27b9e758f8ae10f2a0ba509.

--- pkg/platform/socket/socket_unix.go
+++ pkg/platform/socket/socket_unix.go
@@ -6,7 +6,6 @@ import (
 	"net"
 	"os"
 	"path"
-	"syscall"
 
 	"goauthentik.io/platform/pkg/platform/pstr"
 )
@@ -20,16 +19,12 @@ func listen(spath pstr.PlatformString, perm SocketPermMode) (InfoListener, error
 	case SocketEveryone:
 		uperm = 0666
 	}
-	err := os.MkdirAll(path.Dir(p), os.FileMode(uperm|0100))
+	err := os.MkdirAll(path.Dir(p), os.FileMode(uperm+0100))
 	if err != nil {
 		return nil, err
 	}
 	_ = os.Remove(p)
-	// Restrict umask before socket creation to eliminate the TOCTOU window where
-	// the socket would briefly exist with permissive default permissions.
-	oldUmask := syscall.Umask(0777 &^ uperm)
 	lis, err := net.Listen("unix", p)
-	syscall.Umask(oldUmask)
 	if err != nil {
 		return nil, err
 	}
