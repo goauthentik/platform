aabf41945d28a18cbaa0e9c947e32f9c491786cf
Jens Langhammer <jens.langhammer@beryju.org>
2026-05-24 15:25:21 +0200

    pam: remove token auth

--- e2e/auth_test.go
+++ e2e/auth_test.go
@@ -41,31 +41,3 @@ func Test_Auth_IdentityAgent(t *testing.T) {
 		},
 	})
 }
-
-func Test_Auth_Legacy(t *testing.T) {
-	net, err := network.New(t.Context(), network.WithAttachable())
-	defer testcontainers.CleanupNetwork(t, net)
-	assert.NoError(t, err)
-
-	tc := testMachine(t)
-
-	assert.NoError(t, tc.Start(t.Context()))
-	JoinDomain(t, tc)
-	AgentSetup(t, tc)
-
-	MustExec(t, tc, "sed -i 's/KbdInteractiveAuthentication no/KbdInteractiveAuthentication yes/g' /etc/ssh/sshd_config")
-	MustExec(t, tc, "systemctl restart ssh")
-
-	cmdTest(t, tc, []cmdTestCase{
-		{
-			name:    "ssh_env",
-			cmd:     "ak ssh -i akadmin@$(hostname) env",
-			expects: []string{"AUTHENTIK_CLI_SOCKET", "SSH_CONNECTION"},
-		},
-		{
-			name:    "ssh_ak_whoami",
-			cmd:     "ak ssh -i akadmin@$(hostname) ak whoami",
-			expects: []string{"akadmin"},
-		},
-	})
-}
