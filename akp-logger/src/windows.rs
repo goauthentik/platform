8fe3bbad1b47ae83e345faa043631dbbc7647142
Jens Langhammer <jens.langhammer@beryju.org>
2026-06-06 22:06:27 +0200

    fix log build?

--- akp-logger/src/windows.rs
+++ akp-logger/src/windows.rs
@@ -1,13 +1,5 @@
+use eventlog::init;
+
 pub fn init_log(name: &str) {
     eventlog::init(name, log::Level::Trace).unwrap();
-    let logger = match syslog::unix(formatter) {
-        Ok(logger) => logger,
-        Err(e) => {
-            println!("impossible to connect to syslog: {e:?}");
-            return;
-        }
-    };
-    log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
-        .map(|()| log::set_max_level(LevelFilter::Trace))
-        .unwrap_or(());
 }
