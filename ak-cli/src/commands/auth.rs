44e0190b9bcc0a0a57a8b8342aa079b7da58acda
Jens Langhammer <jens.langhammer@beryju.org>
2026-06-07 19:52:39 +0200

    slightly different

--- ak-cli/src/commands/auth.rs
+++ ak-cli/src/commands/auth.rs
@@ -16,7 +16,7 @@ pub enum AuthCommands {
         client_id: String,
         #[arg(short, long, required = true)]
         role_arn: String,
-        #[arg(short, long, required = true)]
+        #[arg(short = 'R', long, required = true)]
         region: String,
     },
     /// Authenticate to a Kubernetes Cluster with the authentik profile.
