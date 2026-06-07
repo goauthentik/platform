a1ce0f97428f4685aade1973333d6fad3fa4ced5
Jens Langhammer <jens.langhammer@beryju.org>
2026-06-07 19:37:46 +0200

    Revert "add manpage"
    This reverts commit f87b7627b8663ac49cdd93b8c508694580c7d758.

--- ak-cli/src/main.rs
+++ ak-cli/src/main.rs
@@ -1,10 +1,54 @@
-use ak_cli::{
-    Cli, Commands,
-    commands::{auth::AuthCommands, config::ConfigCommands},
-};
 use akp_logger::set_log_level;
-use clap::{Error, Parser};
+use clap::{Error, Parser, Subcommand};
 use log::LevelFilter;
+
+use crate::commands::{auth::AuthCommands, config::ConfigCommands};
+
+pub mod auth;
+pub mod cache;
+pub mod commands;
+pub mod format;
+
+#[derive(Parser)]
+#[command(name = "authentik CLI")]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct Cli {
+    /// Enable debug logging
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
+    /// Output JSON data
+    #[arg(short, long, default_value_t = false)]
+    json: bool,
+    /// A name for the profile
+    #[arg(short, long, default_value = "default")]
+    profile: String,
+    /// Socket the agent is listening on
+    #[arg(short, long)]
+    socket: Option<String>,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Check user account details for a given profile
+    Whoami,
+    /// Version of authentik Agent components
+    Version,
+
+    /// Configure authentik CLI
+    Config {
+        #[command(subcommand)]
+        command: ConfigCommands,
+    },
+    /// Commands for authenticating with different CLI applications.
+    Auth {
+        #[command(subcommand)]
+        command: AuthCommands,
+    },
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -16,23 +60,19 @@ async fn main() -> Result<(), Error> {
     }
 
     let res = match &cli.command {
-        Commands::Whoami => ak_cli::commands::whoami::whoami(&cli).await,
-        Commands::Version => ak_cli::commands::version::version(&cli).await,
+        Commands::Whoami => commands::whoami::whoami(&cli).await,
+        Commands::Version => commands::version::version(&cli).await,
         Commands::Config { command } => match command {
-            ConfigCommands::ListProfiles => ak_cli::commands::config::list_profiles(&cli).await,
+            ConfigCommands::ListProfiles => commands::config::list_profiles(&cli).await,
         },
         Commands::Auth { command } => match command {
-            AuthCommands::Raw { client_id } => {
-                ak_cli::commands::auth::raw(&cli, client_id).await
-            }
-            AuthCommands::Kubectl { client_id } => {
-                ak_cli::commands::auth::kubectl(&cli, client_id).await
-            }
+            AuthCommands::Raw { client_id } => commands::auth::raw(&cli, client_id).await,
+            AuthCommands::Kubectl { client_id } => commands::auth::kubectl(&cli, client_id).await,
             AuthCommands::Aws {
                 client_id,
                 role_arn,
                 region,
-            } => ak_cli::commands::auth::aws(&cli, client_id, role_arn, region).await,
+            } => commands::auth::aws(&cli, client_id, role_arn, region).await,
         },
     };
     match res {
