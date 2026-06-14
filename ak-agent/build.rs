bb020037cdba8fab197aa2025793827c1590798c
Jens Langhammer <jens.langhammer@beryju.org>
2026-06-14 02:34:24 +0200

    maybe fix windows build?

--- ak-agent/build.rs
+++ ak-agent/build.rs
@@ -1,14 +1,13 @@
 extern crate windows_exe_info;
 
 fn main() {
-    #[cfg(windows)]
-    {
-        windows_exe_info::icon::icon_ico("vpkg/windows/resources/icon.ico");
-        windows_exe_info::versioninfo::VersionInfo::from_cargo_env_ex(
-            Some("authentik Platform Agent"),
-            Some("Authentik Security Inc."),
-            Some("2026 Authentik Security Inc."),
-            None
-        ).link().unwrap();
-    }
+    windows_exe_info::icon::icon_ico("../vpkg/windows/resources/icon.ico");
+    windows_exe_info::versioninfo::VersionInfo::from_cargo_env_ex(
+        Some("authentik Platform Agent"),
+        Some("Authentik Security Inc."),
+        Some("2026 Authentik Security Inc."),
+        None,
+    )
+    .link()
+    .unwrap();
 }
