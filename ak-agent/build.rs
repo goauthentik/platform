711aa4e86c2199fcd72633f2ac50dc6dd9d214a9
Jens Langhammer <jens.langhammer@beryju.org>
2026-06-14 13:56:22 +0200

    fix lint

--- ak-agent/build.rs
+++ ak-agent/build.rs
@@ -2,12 +2,14 @@ extern crate windows_exe_info;
 
 fn main() {
     windows_exe_info::icon::icon_ico("../vpkg/windows/resources/icon.ico");
-    windows_exe_info::versioninfo::VersionInfo::from_cargo_env_ex(
+    if let Err(e) = windows_exe_info::versioninfo::VersionInfo::from_cargo_env_ex(
         Some("authentik Platform Agent"),
         Some("Authentik Security Inc."),
         Some("2026 Authentik Security Inc."),
         None,
     )
     .link()
-    .unwrap();
+    {
+        println!("cargo::error={}", e);
+    }
 }
