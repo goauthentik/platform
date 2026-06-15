b6b904553d75b1c245f1e21ad6ef278fd691ca53
Jens Langhammer <jens.langhammer@beryju.org>
2026-06-16 01:15:37 +0200

    ak-platform-authz: backport improvements from agent

--- ak-platform-authz/examples/authz_prompt.rs
+++ ak-platform-authz/examples/authz_prompt.rs
@@ -1,9 +1,21 @@
-use ak_platform::{log::init_log_interactive, string::PlatformString};
-use ak_platform_authz::prompt;
+use std::time::Duration;
+
+use ak_platform::{
+    log::init_log_interactive, net::server::creds::ProcCredentials, string::PlatformString,
+};
+use ak_platform_authz::AuthorizeAction;
 
 #[tokio::main]
 async fn main() {
     init_log_interactive();
-    let res = prompt(PlatformString::new_with_default("authz prompt")).await;
+    let creds = ProcCredentials::current();
+    let res = AuthorizeAction {
+        message: Box::new(|_| Ok(PlatformString::new_with_default("authz prompt"))),
+        uid: Box::new(|_| Ok("static".to_string())),
+        timeout_success: Duration::from_hours(1),
+        timeout_denied: Duration::from_mins(5),
+    }
+    .prompt(creds)
+    .await;
     eprintln!("Authz result: {res:?}");
 }
---
+++ ak-platform-authz/src/sys/mod.rs
@@ -0,0 +1,20 @@
+use ak_platform::prelude::*;
+use ak_platform::string::PlatformString;
+
+#[cfg(target_os = "macos")]
+pub mod macos;
+
+#[cfg(target_os = "linux")]
+pub mod linux;
+
+#[cfg(target_os = "windows")]
+pub mod windows;
+
+pub async fn prompt(msg: PlatformString) -> Result<bool> {
+    #[cfg(target_os = "macos")]
+    return macos::prompt(msg.for_current()).await;
+    #[cfg(target_os = "linux")]
+    return linux::prompt(msg).await;
+    #[cfg(target_os = "windows")]
+    return windows::prompt(msg).await;
+}
