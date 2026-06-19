b96eeef76e427bcfd7782ae25914c3141c57b884
Jens Langhammer <jens.langhammer@beryju.org>
2026-06-19 22:51:19 +0200

    include windows exe info in desktop agent

--- ak-agent-desktop/src-tauri/build.rs
+++ ak-agent-desktop/src-tauri/build.rs
@@ -1,5 +1,18 @@
 fn main() {
     #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-arg=-Wl,-rpath,/usr/lib/swift");
+
+    windows_exe_info::icon::icon_ico("../../vpkg/windows/resources/icon.ico");
+    if let Err(e) = windows_exe_info::versioninfo::VersionInfo::from_cargo_env_ex(
+        Some("authentik Platform Agent"),
+        Some("Authentik Security Inc."),
+        Some("2026 Authentik Security Inc."),
+        None,
+    )
+    .link()
+    {
+        println!("cargo::error={}", e);
+    }
+
     tauri_build::build()
 }
