da0c200b3eb6963cc75c835bb7fe4cbe6547a1e4
Jens Langhammer <jens.langhammer@beryju.org>
2026-07-05 16:19:37 +0100

    log filtering

--- ak-platform/src/log/linux.rs
+++ ak-platform/src/log/linux.rs
@@ -1,34 +1,38 @@
 use std::fs::File;
 
+use env_filter::{Filter, FilteredLog};
 use log::LevelFilter;
 use simplelog::{Config, WriteLogger};
 use syslog::BasicLogger;
 use syslog::{Facility, Formatter3164};
 
-pub fn init_log(name: &str) {
+pub fn init_log(name: &str, filter: Filter) {
     let formatter = Formatter3164 {
         facility: Facility::LOG_USER,
         hostname: None,
         process: name.into(),
         pid: std::process::id(),
     };
-    let logger = match syslog::unix(formatter) {
-        Ok(logger) => logger,
+    let inner: Box<dyn log::Log> = match syslog::unix(formatter) {
+        Ok(logger) => Box::new(BasicLogger::new(logger)),
         Err(e) => {
-            init_file_log(format!("/var/log/authentik/{}.log", name));
-            log::warn!("unable to connect to syslog: {e:?}");
-            return;
+            eprintln!("unable to connect to syslog: {e:?}");
+            match build_file_log(format!("/var/log/authentik/{}.log", name)) {
+                Some(logger) => logger,
+                None => return,
+            }
         }
     };
-    log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
+    log::set_boxed_logger(Box::new(FilteredLog::new(inner, filter)))
         .map(|()| log::set_max_level(LevelFilter::Trace))
         .unwrap_or(());
 }
 
-fn init_file_log(path: String) {
-    let file = match File::create(path) {
-        Ok(f) => f,
-        Err(_) => return,
-    };
-    let _ = WriteLogger::init(LevelFilter::Trace, Config::default(), file);
+fn build_file_log(path: String) -> Option<Box<dyn log::Log>> {
+    let file = File::create(path).ok()?;
+    Some(WriteLogger::new(
+        LevelFilter::Trace,
+        Config::default(),
+        file,
+    ))
 }
