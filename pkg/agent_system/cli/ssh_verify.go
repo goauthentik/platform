43dd50178033de00b559fa671036e81a8fd8b540
Jens Langhammer <jens.langhammer@beryju.org>
2026-07-10 15:09:39 +0100

    cleanup ssh verify

--- pkg/agent_system/cli/ssh_verify.go
+++ pkg/agent_system/cli/ssh_verify.go
@@ -1,11 +1,13 @@
 package cli
 
 import (
+	"context"
 	"crypto/subtle"
 	"fmt"
 	"strings"
 
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"goauthentik.io/platform/pkg/agent_system/client"
 	"goauthentik.io/platform/pkg/pb"
@@ -22,85 +24,90 @@ const (
 )
 
 var sshVerifyCmd = &cobra.Command{
-	Use:    "ssh-verify",
-	Hidden: true,
+	Use:          "ssh-verify",
+	Hidden:       true,
+	SilenceUsage: true,
 	PreRunE: func(cmd *cobra.Command, args []string) error {
 		return systemlog.MustSetup(pstr.PlatformString{
 			Linux: new("ak-sysd"),
 		}.ForCurrent())
 	},
-	RunE: func(cmd *cobra.Command, args []string) error {
+	Run: func(cmd *cobra.Command, args []string) {
 		l := systemlog.Get()
 		if len(args) < 3 {
-			return errors.New("invalid number of arguments")
+			l.Warning("invalid number of arguments")
+			return
 		}
-
-		certPubkey, _, _, _, err := ssh.ParseAuthorizedKey([]byte(args[2] + " " + args[1]))
+		// user, b64key, type = cmd.Args
+		err := validate(cmd.Context(), l, args[0], args[1], args[2])
 		if err != nil {
-			return err
+			l.WithError(err).Warning("failed to verify ssh cert")
 		}
-		sshCert, ok := certPubkey.(*ssh.Certificate)
-		if !ok {
-			return fmt.Errorf("parsed SSH authorized_key is not an SSH certificate (got type %T, cert type %q)", certPubkey, args[2])
-		}
-
-		extHostKey, ok := sshCert.Extensions[ExtAuthentikPlatformSSHHostKey]
-		if !ok {
-			l.Warning("Invalid cert (no host key ext)")
-			return nil
-		}
-		extToken, ok := sshCert.Extensions[ExtAuthentikPlatformSSHToken]
-		if !ok {
-			l.Warning("Invalid cert (no token ext)")
-			return nil
-		}
-
-		// Check host key
-		found := false
-		hks := getLocalHostKeys(common.New(l, cmd.Context()))
-		ghk, _, _, _, err := ssh.ParseAuthorizedKey([]byte(extHostKey))
-		if err != nil {
-			l.WithError(err).Warning("failed to parse ext key")
-			return nil
-		}
-		for _, hk := range hks {
-			if subtle.ConstantTimeCompare(ghk.Marshal(), hk.Marshal()) == 1 {
-				found = true
-			}
-		}
-		if !found {
-			l.Warning("Certificate has wrong host-key")
-			return nil
-		}
-
-		// Check token
-		sc, err := client.NewDefault()
-		if err != nil {
-			l.WithError(err).Warning("failed to connect to ctrl")
-			return nil
-		}
-		res, err := sc.TokenAuth(cmd.Context(), &pb.TokenAuthRequest{
-			Username: args[0],
-			Token:    extToken,
-		})
-		if err != nil {
-			l.WithError(err).Warning("failed to validate token")
-			return nil
-		}
-		if !res.Successful {
-			l.Warning("unsuccessful token validation")
-			return nil
-		}
-
-		pubkeyBytes := strings.TrimSpace(string(ssh.MarshalAuthorizedKey(sshCert.SignatureKey)))
-
-		fmt.Printf("cert-authority,principals=\"%s\" %s\n", res.Token.PreferredUsername, pubkeyBytes)
-		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(sshVerifyCmd)
+}
+
+func validate(ctx context.Context, l *log.Entry, user, b64key, typ string) error {
+	certPubkey, _, _, _, err := ssh.ParseAuthorizedKey([]byte(typ + " " + b64key))
+	if err != nil {
+		return err
+	}
+	sshCert, ok := certPubkey.(*ssh.Certificate)
+	if !ok {
+		return fmt.Errorf("parsed SSH authorized_key is not an SSH certificate (got type %T, cert type %q)", certPubkey, typ)
+	}
+
+	extHostKey, ok := sshCert.Extensions[ExtAuthentikPlatformSSHHostKey]
+	if !ok {
+		return errors.New("Invalid cert (no host key ext)")
+	}
+	extToken, ok := sshCert.Extensions[ExtAuthentikPlatformSSHToken]
+	if !ok {
+		return errors.New("Invalid cert (no token ext)")
+	}
+
+	// Check host key
+	found := false
+	hks := getLocalHostKeys(common.New(l, ctx))
+	ghk, _, _, _, err := ssh.ParseAuthorizedKey([]byte(extHostKey))
+	if err != nil {
+		return errors.Wrap(err, "failed to parse ext key")
+	}
+	for _, hk := range hks {
+		if subtle.ConstantTimeCompare(ghk.Marshal(), hk.Marshal()) == 1 {
+			found = true
+		}
+	}
+	if !found {
+		return errors.New("Certificate has wrong host-key")
+	}
+
+	// Check token
+	sc, err := client.NewDefault()
+	if err != nil {
+		l.WithError(err).Warning("failed to connect to ctrl")
+		return nil
+	}
+	res, err := sc.TokenAuth(ctx, &pb.TokenAuthRequest{
+		Username: user,
+		Token:    extToken,
+	})
+	if err != nil {
+		l.WithError(err).Warning("failed to validate token")
+		return nil
+	}
+	if !res.Successful {
+		l.Warning("unsuccessful token validation")
+		return nil
+	}
+
+	pubkeyBytes := strings.TrimSpace(string(ssh.MarshalAuthorizedKey(sshCert.SignatureKey)))
+
+	fmt.Printf("cert-authority,principals=\"%s\" %s\n", res.Token.PreferredUsername, pubkeyBytes)
+	return nil
 }
 
 func getLocalHostKeys(ctx *common.GatherContext) []ssh.PublicKey {
