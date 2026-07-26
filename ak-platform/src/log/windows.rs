f07f329493a12bdfaa6cca6fe235cff38515bc70
Jens Langhammer <jens.langhammer@beryju.org>
2026-07-26 15:40:04 +0100

    fix log on windows

--- ak-platform/src/log/windows.rs
+++ ak-platform/src/log/windows.rs
@@ -1,7 +1,7 @@
 use env_filter::{Filter, FilteredLog};
 use eventlog::EventLog;
 use eyre::Result;
-use log::LevelFilter;
+use log::Log;
 
 pub fn init_log(name: &str) -> Result<Box<dyn Log>> {
     EventLog::new(name, log::Level::Trace)
