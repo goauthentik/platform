88499ee6a1ae770bd19a01712477c208281e875b
Jens Langhammer <jens.langhammer@beryju.org>
2026-07-27 21:05:14 +0100

    log filter for ak-agent

--- ak-agent/src/main.rs
+++ ak-agent/src/main.rs
@@ -16,6 +16,8 @@ async fn main() -> Result<()> {
             .with_windows("authentik User Service")
             .with_linux("ak-agent"),
     )
+    .default_level(ak_platform::log::LevelFilter::Info)
+    .with_filter("ak_agent", ak_platform::log::LevelFilter::Trace)
     .enable();
     tracing::trace!("authentik Agent v{}", full_version());
     let ag = Agent::new().await?;
